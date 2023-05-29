a1fb9c3bc7c82ff2cf5f170b924b4b41cb20fd99
Dominil0505 <119898747+Dominil0505@users.noreply.github.com>
2023-05-29 21:38:04 +0200

    Files
    Upload Files

---
+++ Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleCrud.Models.Domain;
+
+namespace SimpleCrud.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
